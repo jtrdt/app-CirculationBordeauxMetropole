14fa577bf01904412a5bb85b93785f05153466dc
jtridat <23510328+jtridat@users.noreply.github.com>
2021-04-30 16:59:00 +0200

    feat(table): update table component & some update functions

--- src/components/bouclesComp/table.jsx
+++ src/components/bouclesComp/table.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Table = (props) => {
-  const sendBoucle = async (e) => {
+const Table = props => {
+  const sendBoucle = async e => {
     const id = e.target.id;
-    await fetch(process.env.NEXT_PUBLIC_BOUCLE_URL + '/' + id, {
+    console.log(e.target);
+    await fetch(`${process.env.NEXT_PUBLIC_BOUCLE_URL}/${id}`, {
       method: 'PUT',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -12,14 +13,43 @@ const Table = (props) => {
     });
   };
 
+  const backInService = async e => {
+    const id = e.target.id;
+    await fetch(`${process.env.NEXT_PUBLIC_BOUCLE_URL}/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        backInService: {
+          date: Date.now(),
+          by: '60759e92b67c11354d8c5cfd' // à automatiser
+        }
+      })
+    });
+  };
+
+  const storeBoucle = async e => {
+    const id = e.target.id;
+    await fetch(`${process.env.NEXT_PUBLIC_BOUCLE_URL}/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        isStored: {
+          date: Date.now(),
+          by: '6075849ab67c11354d8c5cfb'
+        }
+      })
+    });
+  };
+
   return (
-    <table className='border m-2 table-auto'>
+    <table className='border m-2 max-w-full'>
       <thead className='m-0 p-1'>
         <tr>
           <th className='border border-black bg-gray-300 p-1'></th>
           <th className='border border-black bg-gray-300 p-1'>date</th>
           <th className='border border-black bg-gray-300 p-1'>nom</th>
           <th className='border border-black bg-gray-300 p-1'>z+c</th>
+          <th className='border border-black bg-gray-300 p-1'>nature</th>
           <th className='border border-black bg-gray-300 p-1'>entrée</th>
           <th className='border border-black bg-gray-300 p-1'>libellée</th>
           <th className='border border-black bg-gray-300 p-1'>commentaire</th>
@@ -30,10 +60,14 @@ const Table = (props) => {
         </tr>
       </thead>
       <tbody>
-        {props.data.map((carf) => (
+        {props.data.map(carf => (
           <tr
             key={carf._id}
-            className='odd:bg-white bg-gray-100 hover:bg-indigo-50'
+            className={
+              carf.isStored
+                ? 'bg-purple-200 hover:bg-indigo-50'
+                : 'odd:bg-white bg-gray-100 hover:bg-indigo-50'
+            }
           >
             <td className='border border-black p-1 text-center'>
               {carf.isUrgent && <span>!!!</span>}
@@ -49,6 +83,9 @@ const Table = (props) => {
               {carf.carfId}
             </td>
             <td className='border border-black p-1 text-center'>
+              {carf.nature}
+            </td>
+            <td className='border border-black p-1 text-center'>
               {carf.entry}
             </td>
             <td className='border border-black p-1 text-center'>
@@ -61,12 +98,20 @@ const Table = (props) => {
               {carf.sendedDate}
             </td>
             <td className='border border-black p-1 text-center'>
-              {carf.backInService}
+              {carf.backInService && (
+                <span>
+                  Remis en service le {carf.backInService.date} par{' '}
+                  {carf.backInService.by.name}
+                </span>
+              )}
             </td>
             <td>
-              <button className='bg-gray-400 border hover:bg-gray-300'>
+              {/* <button
+                className='bg-gray-400 border hover:bg-gray-300'
+                onClick={props.showEditForm}
+              >
                 Editer
-              </button>
+              </button> */}
               <button
                 className='bg-gray-400 border hover:bg-gray-300'
                 onClick={sendBoucle}
@@ -74,6 +119,23 @@ const Table = (props) => {
               >
                 Marquer transmis
               </button>
+              {carf.backInService ? (
+                <button
+                  className='bg-gray-400 border hover:bg-gray-300'
+                  onClick={storeBoucle}
+                  id={carf._id}
+                >
+                  Archiver
+                </button>
+              ) : (
+                <button
+                  className='bg-gray-400 border hover:bg-gray-300'
+                  onClick={backInService}
+                  id={carf._id}
+                >
+                  Remis en service
+                </button>
+              )}
             </td>
           </tr>
         ))}
