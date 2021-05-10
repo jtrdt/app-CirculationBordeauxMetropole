17199e30104178dab5a8681be1bab9e209e37a31
jtridat <23510328+jtridat@users.noreply.github.com>
2021-05-10 11:14:47 +0200

    style(index): update index layout

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import Layout from '../src/components/layout/layout.jsx';
+import SignUpForm from '../src/components/auth/signupForm.jsx';
+import LoginForm from '../src/components/auth/loginForm.jsx';
 
 const Home = () => {
   return (
     <Layout>
-      <h1>Accueil</h1>
+      <SignUpForm />
+      <LoginForm />
     </Layout>
   );
 };
