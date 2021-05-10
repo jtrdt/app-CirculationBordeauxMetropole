d4287039a37ad97e4db7631988bf9f7f0e9a776c
jtridat <23510328+jtridat@users.noreply.github.com>
2021-05-10 11:08:31 +0200

    fix(_document): remove title & meta links

--- pages/_document.js
+++ pages/_document.js
@@ -8,12 +8,7 @@ class MyDocument extends Document {
         The <Head /> component used here should only be used for any <head> code that is common for all pages. 
         For all other cases, such as <title> tags, we recommend using next/head in your pages or components. */}
         <Head>
-          <title>PCBM 🤔</title>
           <meta charSet='utf-8' />
-          <meta
-            name='viewport'
-            content='initial-scale=1.0, width=device-width'
-          />
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
             href='https://fonts.googleapis.com/css2?family=Dosis&display=swap'
