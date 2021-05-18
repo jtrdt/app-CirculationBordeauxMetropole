9b21da49248fce18432f2a1a5240bcf21a6d22e5
jtridat <23510328+jtridat@users.noreply.github.com>
2021-05-18 22:09:10 +0200

    style(footer): prettier

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className='h-16 border-t flex justify-center items-center text-base font-medium text-gray-400 bottom-0 bg-transparent-bar'>
-      <p>Made by <a href='https://github.com/Bill-Pecoss' rel='nofollow'>Bill & Pecoss</a> | Copyright 2021</p>
+      <p>
+        Made by{' '}
+        <a href='https://github.com/Bill-Pecoss' rel='nofollow'>
+          Bill & Pecoss
+        </a>{' '}
+        | Copyright 2021
+      </p>
     </footer>
   );
 };
