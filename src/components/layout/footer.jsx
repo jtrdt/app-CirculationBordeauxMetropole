2dea14fa37298d75856921b67bd31dc2f0d6900b
jtridat <23510328+jtridat@users.noreply.github.com>
2021-05-21 17:22:57 +0200

    style(footer): update css

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='h-16 border-t flex justify-center items-center text-base font-medium text-gray-400 bottom-0 bg-transparent-bar'>
+    <footer className='h-16 border-t flex justify-center items-center text-base font-normal text-gray-400 bottom-0 bg-transparent-bar'>
       <p>
         Made by{' '}
-        <a href='https://github.com/Bill-Pecoss' rel='nofollow'>
+        <a href='https://github.com/Bill-Pecoss' rel='nofollow noopener' className='underline'>
           Bill & Pecoss
         </a>{' '}
         | Copyright 2021
