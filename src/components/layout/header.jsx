19629696815badbe94e4045847e39a1ad8d5634e
jtridat <23510328+jtridat@users.noreply.github.com>
2021-05-21 16:57:40 +0200

    style(header): add link and remove some useless code

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 const Header = () => {
   return (
     <header className='header font-semibold text-white p-7 flex flex-col justify-center items-center h-40 bg-transparent-header'>
-      <img
-        src='BM_logo_positif_CMYK_horiz.png'
-        alt='Logo Bordeaux Métropole couleur'
-        width={283}
-        height={124}
-        className='left-20 absolute'
-      />
-      <h1 className='text-3xl font-medium'>PC Circulation Bordeaux Métropole</h1>
+      <a href='/' className='left-20 absolute'>
+          <img
+            src='../BM_logo_positif_CMYK_horiz.png'
+            alt='Logo Bordeaux Métropole couleur'
+          />
+      </a>
+      <h1 className='text-3xl font-medium'>
+        PC Circulation Bordeaux Métropole
+      </h1>
     </header>
   );
 };
