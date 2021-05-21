6345c21c4dc715a2bed7eb32410bcd559cfba60f
jtridat <23510328+jtridat@users.noreply.github.com>
2021-05-21 16:54:16 +0200

    refactor(auth form): change layout and redirection

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import Layout from '../src/components/layout/layout.jsx';
-import SignUpForm from '../src/components/auth/signupForm.jsx';
 import LoginForm from '../src/components/auth/loginForm.jsx';
 
 const Home = () => {
   return (
     <Layout>
-      <SignUpForm />
       <LoginForm />
     </Layout>
   );
--- pages/signup/index.jsx
+++ pages/signup/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
 import SignUpForm from '../../src/components/auth/signupForm.jsx';
-import LoginForm from '../../src/components/auth/loginForm.jsx';
 import Layout from '../../src/components/layout/layout.jsx';
 
 function Home() {
   return (
     <Layout>
       <SignUpForm />
-      <LoginForm />
     </Layout>
   );
 }
