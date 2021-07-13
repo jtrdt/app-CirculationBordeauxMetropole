bf5db312a9fc2b12527e9104d17b378abb9ef794
kratere <23510328+jtridat@users.noreply.github.com>
2021-07-13 15:13:11 +0200

    feat(reset form): add a reset form after pwd reset

---
+++ pages/password_reset/update/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Head from 'next/head';
+import UpdateResetPassword from '../../../src/components/auth/updateResetPassword';
+
+const Home = () => {
+  return (
+    <div className='bg-home h-screen flex'>
+      <Head>
+        <title>PC Circulation Bordeaux Métropole</title>
+      </Head>
+      <div className='m-auto'>
+        <UpdateResetPassword />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
