6fab99c1b5c7d114e4e396385295efa2cd864014
kratere <23510328+jtridat@users.noreply.github.com>
2021-07-13 15:12:31 +0200

    feat(emailok): feedback window after veryfing email

---
+++ pages/signup/emailok.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home = () => {
+  return (
+    <div className='bg-home h-screen flex'>
+      <Head>
+        <title>PC Circulation Bordeaux Métropole</title>
+      </Head>
+      <div className='m-auto'>
+        <div
+          className='rounded-3xl py-8 px-20 border bg-login'
+          style={{
+            width: '640px',
+            backdropFilter: 'blur(40px)'
+          }}>
+          <div className='text-center'>
+            <Image
+              src={'/BM_logo_positif_CMYK_horiz.png'}
+              alt='Logo Bordeaux Métropole'
+              width='283px'
+              height='124px'
+            />
+          </div>
+          <h3 className='text-2xl text-center pt-3 pb-4 text-white'>
+            Adresse email vérifiée, vous pouvez vous connecter. <br />
+            <a href='/' className='underline text-base'>
+              Revenir à l'acceuil
+            </a>
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
