fc85ff9dd7674408d7e150bedc8f68c024347140
kratere <23510328+kratere@users.noreply.github.com>
2021-08-14 14:22:53 +0200

    feat(404): Initial commit

---
+++ pages/404.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import LogoBdxMetro from '../public/BM_logo_positif_CMYK_horiz.png';
+
+const Home = () => {
+  return (
+    <div className='bg-home h-screen flex'>
+      <Head>
+        <title>PC Circulation Bordeaux Métropole</title>
+      </Head>
+      <div className='m-auto'>
+        <div
+          className='rounded-3xl py-8 px-20 border bg-login'
+          style={{
+            width: '640px',
+            backdropFilter: 'blur(40px)'
+          }}>
+          <div className='text-center'>
+            <Image
+              src={LogoBdxMetro}
+              alt='Logo Bordeaux Métropole'
+              placeholder='blur'
+            />
+          </div>
+          <h3 className='text-2xl text-center pt-3 pb-4 text-white'>
+            Error 404 — Page Not Found
+            <br />
+            <a href='/' className='underline text-base'>
+              Revenir à l'acceuil
+            </a>
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
