47b901591ffff42037927866200ebacc72665495
kratere <23510328+kratere@users.noreply.github.com>
2021-09-04 19:24:34 +0200

    feat(login form): ux - add a loading incon when login in

--- src/components/auth/loginForm.jsx
+++ src/components/auth/loginForm.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import LogoBdxMetro from '../../../public/BM_logo_positif_CMYK_horiz.png';
 import validator from 'validator';
 
 const LoginForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const loginUser = async e => {
     e.preventDefault();
+    setIsLoading(true);
     const login = e.target.name.value.toLowerCase();
     const emailValidator = validator.isEmail(login);
     const error = document.getElementById('errorLogin');
@@ -29,10 +31,12 @@ const LoginForm = () => {
         }
       );
       if (resLogin.status === 403) {
+        setIsLoading(false);
         return (error.innerHTML =
           'Utilisateur non validé, vérifiez vos emails.');
       }
       if (resLogin.status !== 200) {
+        setIsLoading(false);
         return (error.innerHTML = "Erreur d'authentification.");
       }
       {
@@ -44,11 +48,8 @@ const LoginForm = () => {
   };
 
   return (
-    <form onSubmit={loginUser}>
-      <div
-        className='rounded-3xl py-8 px-24 border bg-login backdrop-filter backdrop-blur-3xl'
-        style={{ width: '640px' }}
-      >
+    <section>
+      <div className='rounded-3xl py-8 px-24 border bg-login backdrop-filter backdrop-blur-3xl max-w-2xl'>
         <div className='text-center'>
           <Image
             src={LogoBdxMetro}
@@ -59,27 +60,30 @@ const LoginForm = () => {
         <h3 className='text-2xl text-center pt-3 pb-4 text-white'>
           Poste de commandement Circulation
         </h3>
-        <label htmlFor='name' className='flex flex-col'>
-          <input
-            id='name'
-            name='name'
-            typeof='text'
-            placeholder='Email'
-            className='mt-1 mb-4 rounded-md border-2 px-2 py-3 pl-4 leading-5 border-blue-300 text-black'
-            required
-          />
-        </label>
-        <label htmlFor='password' className='flex flex-col'>
-          <input
-            id='password'
-            name='password'
-            type='password'
-            placeholder='Mot de passe'
-            className='mt-1 mb-8 rounded-md border-2 px-2 py-3 pl-4 leading-5  border-blue-300 text-black'
-            required
-          />
-        </label>
-        <button type='submit' className='hidden' />
+        <form onSubmit={loginUser}>
+          <label htmlFor='name' className='flex flex-col'>
+            <input
+              id='name'
+              name='name'
+              typeof='text'
+              placeholder='Email'
+              className='mt-1 mb-4 rounded-md border-2 px-2 py-3 pl-4 w-96 leading-5 border-blue-300 text-black'
+              required
+            />
+          </label>
+          <label htmlFor='password' className='flex flex-col'>
+            <input
+              id='password'
+              name='password'
+              type='password'
+              placeholder='Mot de passe'
+              className='mt-1 mb-8 rounded-md border-2 px-2 py-3 pl-4 leading-5  border-blue-300 text-black'
+              required
+            />
+            {isLoading ? <button class='btn btn-sm btn-ghost loading' /> : null}
+          </label>
+          <button type='submit' className='hidden' />
+        </form>
         <p id='errorLogin' className='text-red-600 pb-3'></p>
         <div className='border w-64 mx-auto mb-6 border-white'></div>
         <div className='text-center'>
@@ -99,7 +103,7 @@ const LoginForm = () => {
           </p>
         </div>
       </div>
-    </form>
+    </section>
   );
 };
 
